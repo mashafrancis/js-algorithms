81cfc85863f81da189f8b80c01df3984db8620f1
Francis Masha <francismasha96@gmail.com>
2020-01-13 09:58:50 +0300

    add functions

---
+++ add2_timing.js
@@ -0,0 +1,9 @@
+function addUpTo(n) {
+    return n * (n + 1)/2;
+}
+
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+
+console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`);
